133ce84dfec89a76cd7ccebe56b3548ec870dfaf
Caio Magalhães <caio.lmf@gmail.com>
2020-07-16 12:53:22 -0300

    Add Feedback page and route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
 import store from './redux/store';
 import App from './App';
 import './index.css';
@@ -8,12 +10,12 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />,
-  </Provider>
-  , document.getElementById('root'),
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    ,
+  </Provider>,
+  document.getElementById('root'),
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
---
+++ src/pages/FeedbackScreen/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class FeedbackScreen extends Component {
+  render() {
+    return (
+      <div>
+        <p>teste ok</p>
+      </div>
+    );
+  }
+}
+
+export default FeedbackScreen;
